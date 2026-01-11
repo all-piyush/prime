b73e819924a7654852dea1f00851750d4540414a
all-piyush <piyusharora732@gmail.com>
2026-01-11 16:01:32 +0530

    Revert "third commit"
    This reverts commit 1e4e308dc89fe35c0c9d2c77926b15fb40f3ff15.

--- appback/server.js
+++ appback/server.js
@@ -5,13 +5,15 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-  origin:'https://prime-4fht.onrender.com/api/v1',credentials:true}
+  origin:'https://prime-4fht.onrender.com',credentials:true}
 ));
 app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World from Express!');
 });
-
+app.listen(4000, () => {
+    console.log(`Server is running on http://localhost:4000`);
+});
 const dbconnect=require('./configuration/database');
 dbconnect();
 const userroutes=require('./Routes/Route');
